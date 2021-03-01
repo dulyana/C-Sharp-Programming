555fb49e7ba79084507f2b8b3bb0ea4ba431ce30
Dulyana  Gunawardhana <67815837+dulyana@users.noreply.github.com>
2021-03-02 01:27:31 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿using System;
+﻿
+using System;
 
-namespace practical_1
+namespace _1Tutorial02
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int number1;
+            int number2;
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Number 01 :");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number 02 :");
+            number2 = int.Parse(Console.ReadLine());
+
+            int sum = number1 + number2;
+            int subtraction = number1 - number2;
+            int multiplication = number1 * number2;
+            double division = Convert.ToDouble(number1) / Convert.ToDouble(number2); // type casting Converting int to double
+
+            Console.WriteLine("Summation is " + sum);
+            Console.WriteLine("Multiplication is " + multiplication);
+            Console.WriteLine("Subtraction is " + subtraction);
+            Console.WriteLine("Division is " + division);
+
+            Console.ReadKey();
         }
+
+
     }
+}
 }
